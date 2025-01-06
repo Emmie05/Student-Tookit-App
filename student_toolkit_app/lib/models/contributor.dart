592c0f031bff3f54dee72d4fbf5f438cb03fb444
Emmie05 <emmvicadforster@gmail.com>
2025-01-06 01:22:37 +0000

    Add Contributor model with properties for name, department, major, image path, and social links

---
+++ student_toolkit_app/lib/models/contributor.dart
@@ -0,0 +1,15 @@
+class Contributor {
+  final String name;
+  final String department;
+  final String major;
+  final String imagePath;
+  final Map<String, String> socialLinks;
+
+  Contributor({
+    required this.name,
+    required this.department,
+    required this.major,
+    required this.imagePath,
+    required this.socialLinks,
+  });
+}
