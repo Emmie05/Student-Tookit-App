83cf282fdde3130c8a2454b8124cfd9e09e4cb17
Emmie05 <emmvicadforster@gmail.com>
2025-01-06 01:22:48 +0000

    Add Course model with properties for name, grade, and credit hours

---
+++ student_toolkit_app/lib/models/course.dart
@@ -0,0 +1,11 @@
+class Course {
+  final String name;
+  final String grade;
+  final double creditHour;
+
+  Course({
+    required this.name,
+    required this.grade,
+    required this.creditHour,
+  });
+}
