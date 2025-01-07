3dc304a7b957b1877c671383d218d6b5edd23067
Emmie05 <emmvicadforster@gmail.com>
2025-01-07 16:00:07 +0000

    Update Contributor model to replace major with university and course fields

--- student_toolkit_app/lib/models/contributor.dart
+++ student_toolkit_app/lib/models/contributor.dart
@@ -1,14 +1,16 @@
 class Contributor {
   final String name;
   final String department;
-  final String major;
+  final String university;
+  final string course;
   final String imagePath;
   final Map<String, String> socialLinks;
 
   Contributor({
     required this.name,
     required this.department,
-    required this.major,
+    required this.university,
+    required this.course,
     required this.imagePath,
     required this.socialLinks,
   });
